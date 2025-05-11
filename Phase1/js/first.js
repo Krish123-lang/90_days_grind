3b7444a7bf9336432c936cb7690a500cc6961bc9
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2025-05-11 17:16:07 +0000

    js

--- Phase1/js/first.js
+++ Phase1/js/first.js
@@ -1,3 +1,17 @@
-let username = "Krishna"
+/*let username = "Krishna"
 let address = "biratnagar"
 console.log(`Hello, ${username}. You are from ${address}`);
+*/
+
+const greet = (user_name) => {
+    console.log(`Hello, ${user_name}`)
+}
+// greet('Krishna mandal')
+
+
+for (let i = 0; i <= 10; i++) {
+    // console.log("i: ", i)
+    if (i % 2 == 0) {
+        console.log("i: ", i) // even numbers
+    }
+}
