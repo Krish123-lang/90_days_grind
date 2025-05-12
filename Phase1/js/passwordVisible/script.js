4a70e33ed08cfd90f5ca56c605cfc2e30ce8efb4
Krishna Mandal <mandalkrishna739@gmail.com>
2025-05-12 19:34:40 +0545

    password toggle

--- Phase1/js/passwordVisible/script.js
+++ Phase1/js/passwordVisible/script.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const password = document.getElementById('password')
+const passToggle = document.getElementById('toggle')
+
+passToggle.addEventListener('click', function () {
+    const type = password.getAttribute("type") === "password" ? "text" : "password"
+    password.setAttribute("type", type)
+
+    this.classList.toggle('toggle')
+})
+
+const form = document.querySelector("form")
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+})
