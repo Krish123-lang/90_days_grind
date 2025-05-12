4e3a13687efadf1435fc9d9d591996bd50214593
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2025-05-12 05:24:44 +0000

    js basics

---
+++ Phase1/js/html/script.js
@@ -0,0 +1,38 @@
+// alert("Hello")
+// console.log("Hello")
+// let name = prompt("Enter your name: ")
+// console.log("krishna")
+
+// *** querySelector() ***
+const h1 = document.querySelector('h1')
+// console.log(h1.textContent) // displays text content
+
+// h1.textContent = "Krishna mandal" // changes text content
+
+// --------------------------------
+
+// *** getElementById() ***
+
+const paraId = document.getElementById("para1")
+// console.log(paraId.innerHTML)
+// console.log(paraId.innerText)
+
+// --------------------------------
+
+// *** getElementsByName() ***
+const getByName = document.getElementsByName("language")
+// console.log(getByName.length)
+
+// ----------------------------------
+
+// *** getElementsByTagName() ***
+const getTagName = document.getElementsByTagName("footer")
+// console.log(getTagName.item(0))
+
+// ----------------------------------
+
+// *** getElementsByClassName() ***
+const getElementClassName = document.getElementsByClassName("box1")
+// console.log(getElementClassName.length);
+
+// ----------------------------------
