09a86aa0d88a552f1556994a069bd1289f189684
Krishna Mandal <mandalkrishna739@gmail.com>
2025-05-12 20:45:04 +0545

    python init

---
+++ Phase1/pythonTut/main.py
@@ -0,0 +1,20 @@
+'''for i in range(10):
+    if (i % 2 == 0):
+        print(i)
+    # print("i: ", i)
+'''
+
+
+def greet(name):
+    return f"Hello, {name}"
+
+
+# name = input("enter your name: ")
+# print(greet(name))
+
+
+'''
+test=lambda x:x*x
+print(test(5)) # 25
+'''
+
