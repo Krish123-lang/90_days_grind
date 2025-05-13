772e25f94e5237664cfb55663d3897bbd5ec2471
Krishna Mandal <mandalkrishna739@gmail.com>
2025-05-13 23:16:14 +0545

    django init

--- Phase2/blog/app/views.py
+++ Phase2/blog/app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+posts = [
+    {
+        'title': 'Beautiful is better than ugly',
+        'author': 'John Doe',
+        'content': 'Beautiful is better than ugly',
+        'published_at': 'October 1, 2022'
+    },
+    {
+        'title': 'Explicit is better than implicit',
+        'author': 'Jane Doe',
+        'content': 'Explicit is better than implicit',
+        'published_at': 'October 1, 2022'
+    }
+]
+
+
+def home(request) -> HttpResponse:
+    # return HttpResponse('<h1>Blog Home</h1>')
+    context={'posts': posts, 'title': 'Django is amazing'}
+    return render(request, 'app/index.html', context)
+
+def about(request):
+    return render(request, 'app/about.html')
---
+++ Phase2/blog/static/js/app.js
@@ -0,0 +1,5 @@
+// setTimeout(() => {
+//     alert("Welcome to my app")
+// }, 3000)
+
+console.log('it works');
