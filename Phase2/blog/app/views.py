aa4843c8162069f4cf3919e8c27d767be2740045
Krishna Mandal <mandalkrishna739@gmail.com>
2025-05-14 08:02:44 +0545

    django admin

--- Phase2/blog/app/views.py
+++ Phase2/blog/app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
-
+'''
 posts = [
     {
         'title': 'Beautiful is better than ugly',
@@ -16,12 +17,14 @@ posts = [
         'published_at': 'October 1, 2022'
     }
 ]
+'''
 
 
 def home(request) -> HttpResponse:
-    # return HttpResponse('<h1>Blog Home</h1>')
-    context={'posts': posts, 'title': 'Django is amazing'}
+    posts = Post.objects.all()
+    context = {'posts': posts, 'title': 'Django is amazing'}
     return render(request, 'app/index.html', context)
+
 
 def about(request):
     return render(request, 'app/about.html')
