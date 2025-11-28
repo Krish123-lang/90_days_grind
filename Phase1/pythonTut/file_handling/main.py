30e6120726d359f63429dadf6dc189f1e6481ad0
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2025-11-28 02:24:37 +0000

    file handling in python

---
+++ Phase1/pythonTut/file_handling/main.py
@@ -0,0 +1,3 @@
+with open('/workspaces/90_days_grind/Phase1/pythonTut/file_handling/readme.txt') as w:
+    r=w.readlines()
+    print(r)
---
+++ Phase1/pythonTut/file_handling/readme.txt
@@ -0,0 +1,3 @@
+this is a readme.txt file.
+made only for testing purpose!
+This is just an epic piece of shit !
