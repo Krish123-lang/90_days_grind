d4dae205720c9a05b53bf18750ae4683f0df0f44
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2025-12-02 04:02:21 +0000

    Edit form

--- Phase2/testi/blog/urls.py
+++ Phase2/testi/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("post/create", views.create_post, name="create_post"),
+    path("post/edit/<int:id>", views.edit_post, name="edit_post"),
     path("about", views.about, name="about"),
 ]
--- Phase2/testi/blog/views.py
+++ Phase2/testi/blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .forms import BlogForm
 from .models import BlogModel
 from django.contrib import messages
@@ -27,5 +27,20 @@ def create_post(request):
             messages.error(request, "Something went wrong!")
             return render(request, "blog/create_post.html", {"form": form})
 
+def edit_post(request, id):
+    posts=get_object_or_404(BlogModel, id=id)
+    if request.method == "GET":
+        context={"form": BlogForm(instance=posts), 'id':id}
+        return render(request, "blog/create_post.html", context)
+    elif request.method == "POST":
+        form=BlogForm(request.POST, instance=posts)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Post edited Successfully!")
+            return redirect('home')
+        else:
+            messages.error(request, "Something went wrong!")
+            return render(request, "blog/create_post.html", {"form": form})
+    
 def about(request):
     return render(request, "blog/about.html")
