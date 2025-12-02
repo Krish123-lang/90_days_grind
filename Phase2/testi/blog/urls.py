762a2e63f2f679d3ead8daf3d33ea7bc89499288
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2025-12-02 02:27:58 +0000

    django forms Posts

--- Phase2/testi/blog/urls.py
+++ Phase2/testi/blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("post/create", views.create_post, name="create_post"),
     path("about", views.about, name="about"),
 ]
--- Phase2/testi/blog/views.py
+++ Phase2/testi/blog/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .forms import BlogForm
 from .models import BlogModel
 
 # Create your views here.
@@ -12,6 +12,17 @@ def home(request):
         "title": "The zen of python",    }
     return render(request, "blog/home.html", context)
 
+def create_post(request):
+    if request.method == "GET":
+        form=BlogForm()
+        return render(request, "blog/create_post.html", {'form': form})
+    elif request.method == "POST":
+        form=BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            return render(request, "blog/create_post.html", {"form": form})
 
 def about(request):
     return render(request, "blog/about.html")
